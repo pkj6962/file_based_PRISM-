ad9eed19d3ad10c7b1167ef3f23f7fea139b3f1b
junghwan <pkj6962@gmail.com>
2023-09-11 15:35:24 +0900

    load-balance-test done

--- mpitracer.cc
+++ mpitracer.cc
@@ -759,7 +759,9 @@ namespace danzer
 
                 // TODO: If file size is over 2GB, we should iteratively read file to the end. 
                 uint64_t remaining_file_size = file_size; 
-                for(uint64_t offset = 0; offset < file_size; offset += 2147479552)
+                uint64_t max_read_size = 104857600;
+				
+				for(uint64_t offset = 0; offset < file_size; offset += max_read_size)
                 {
                     buffer = &bufferpool[reader_idx]; 
                  
@@ -769,7 +771,7 @@ namespace danzer
                         pthread_cond_wait(&buffer->cond, &buffer->mutex); 
                     }
 
-                    uint64_t buffer_size = (2147479552 < remaining_file_size)? 2147479552:remaining_file_size; 
+                    uint64_t buffer_size = (max_read_size < remaining_file_size)? max_read_size:remaining_file_size; 
                     buffer->data = (char*)malloc(buffer_size);
                     if (buffer->data == NULL)
                     {
@@ -855,6 +857,16 @@ namespace danzer
             if (shutdown_flag && !taskFound)
             {
                 reader_done = true; 
+				
+				for (Buffer &b : bufferpool)
+				{
+					pthread_mutex_lock(&b.mutex); 
+					pthread_cond_signal(&b.cond); 
+					pthread_mutex_unlock(&b.mutex); 
+					if (b.filled)
+						printf("Some buffers are filled\n"); 
+				}
+
                 printf("reader done: %d\t\n", rank);
                 printf("rank%d\topen%d\n", rank, test_open_cnt); 
                 break; 
